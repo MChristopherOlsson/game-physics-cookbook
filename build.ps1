8355c6cb11ff0eb47081fa85e5cbe21a79f1ab12
Christopher Olsson <m.christopher.olsson@gmail.com>
2026-06-06 11:20:06 +0200

    Add buildscript

---
+++ build.ps1
@@ -0,0 +1,10 @@
+# Compile
+# -Wall = turns on compiler warnings
+# -Wextra = turns on more warnings
+# -std = sets the c++ standard the compiler is using
+g++ -Wall -Wextra -std=c++17 *.cpp -o main.exe
+
+# Run if compiled successfully
+if ($LASTEXITCODE -eq 0) {
+  .\main.exe
+}
